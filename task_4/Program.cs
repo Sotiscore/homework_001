d3064117de02467b126c255d7233a0dea548e19a
Sotiscore <achernov1188@gmail.com>
2023-09-25 16:53:11 +0400

    task_4 completed

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+int max = 0;
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if(a > max)
+{
+    max = a;
+}
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+
+Console.WriteLine("max = " + max);
